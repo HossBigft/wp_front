730563118550e8b68e901ffb46d8ca579c589953
Maksim Gluwenko <maksim_gluwenko@epam.com>
2025-01-23 20:54:57 +0500

    Update endpoint route

--- src/ShowSearchFilter.jsx
+++ src/ShowSearchFilter.jsx
@@ -3,18 +3,6 @@ import axios from "axios";
 
 axios.defaults.baseURL = "http://localhost:8000";
 
-axios.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("access_token");
-    if (token) {
-      config.headers["Authorization"] = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
 
 const ShowSearchFilter = ({ filter, setFilter, setMovies }) => {
   const [type, setType] = useState(filter.type || "");
@@ -54,7 +42,7 @@ const ShowSearchFilter = ({ filter, setFilter, setMovies }) => {
     const newFilter = buildFilter();
     setFilter(newFilter);
     try {
-      const response = await axios.post("/movies", newFilter);
+      const response = await axios.post("/shows", newFilter);
       console.log(response.data);
       setMovies(response.data);
     } catch (error) {
